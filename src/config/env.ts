4c22b481b8a1c6581ce49c320ea1c6a8a5d55a39
ANDRIANAIVO Nafafisoa <166216261+NaffyLien@users.noreply.github.com>
2026-08-14 20:49:05 +0300

    sdvfbnm

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,2 +1,2 @@
-export const VITE_ROUTE = import.meta.env.VITE_route ?? 'pocketify/'
+export const VITE_ROUTE = import.meta.env.VITE_route ?? 'petitify/'
 export const VITE_GOOGLE_CLIENT_ID = import.meta.env.VITE_VOTRE_CLIENT_ID ?? 'something-inspiring-you-on-friday-with-a-beer'
