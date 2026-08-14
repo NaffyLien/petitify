382b715c19648deed817a2c505186e847ae2df31
ANDRIANAIVO Nafafisoa <nafafyandrianaivo@gmail.com>
2026-08-14 19:02:46 +0300

    feat: implementing google connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Outlet } from 'react-router-dom'
 import Home from './pages/Home'
 import ExportPage from './pages/ExportPage'
 import { useResume } from './contexts/useResume'
 import HelloWorld from './pages/HelloWorld'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const AuthWrapper = ({ children }: { children: React.ReactNode }) => (
+  <GoogleOAuthProvider clientId="VOTRE_CLIENT_ID.apps.googleusercontent.com">
+    {children}
+  </GoogleOAuthProvider>
+)
 
 const AppRoutes = () => {
   const { resume } = useResume()
@@ -11,9 +18,11 @@ const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route path='/pocketify/' element={<HelloWorld/>} />
-      <Route path={`/pocketify/redac`} element={<Home />} />
-      <Route path={`/pocketify/walk`} element={<ExportPage resume={resume} />} />
+      <Route path='/pocketify/' element={<HelloWorld />} />
+      <Route element={<AuthWrapper><Outlet /></AuthWrapper>}>
+        <Route path={`/pocketify/redac`} element={<Home />} />
+        <Route path={`/pocketify/walk`} element={<ExportPage resume={resume} />} />
+      </Route>
     </Routes>
   )
 }
